ec10763f9f2ee9d52b41ffdef48e187980e4c75b
mahmoudyoussef3 <hhfjfjfj35@gmail.com>
2026-02-15 15:37:13 +0200

    fix: adjust SafeArea settings in QiblahFinderScreen for improved layout

--- lib/features/qiblah_finder/ui/qiblah_finder_screen.dart
+++ lib/features/qiblah_finder/ui/qiblah_finder_screen.dart
@@ -44,6 +44,7 @@ class _QiblahFinderScreenState extends State<QiblahFinderScreen> {
       child: Scaffold(
         backgroundColor: ColorsManager.primaryBackground,
         body: SafeArea(
+          top: false,
           bottom: true,
           child: BlocBuilder<QiblahCubit, QiblahState>(
             builder: (context, state) {
